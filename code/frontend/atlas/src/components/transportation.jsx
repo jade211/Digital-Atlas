76db7a92fc5ee4197261166377f4e56e75a5b1ff
Sruthi Santhosh <sruthi.santhosh2@mail.dcu.ie>
2024-02-08 23:49:51 +0000

    fixed transportation.jsx

--- code/frontend/atlas/src/components/transportation.jsx
+++ code/frontend/atlas/src/components/transportation.jsx
@@ -1,21 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
-function Transport() {
-  // return (
-    const [data, setData] = useState(null);
+function Transport(){
+  const [transports, setTransports] = useState([]);
 
-    useEffect(() => {
-      fetch('https://ws.cso.ie/public/api.jsonrpc?data=%7B%22jsonrpc%22:%222.0%22,%22method%22:%22PxStat.Data.Cube_API.ReadDataset%22,%22params%22:%7B%22class%22:%22query%22,%22id%22:%5B%5D,%22dimension%22:%7B%7D,%22extension%22:%7B%22pivot%22:null,%22codes%22:false,%22language%22:%7B%22code%22:%22en%22%7D,%22format%22:%7B%22type%22:%22JSON-stat%22,%22version%22:%222.0%22%7D,%22matrix%22:%22THA24%22%7D,%22version%22:%222.0%22%7D%7D')
-      .then((response) => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error));
-    }, []);
+  useEffect(() =>{
+    fetch(`http://127.0.0.1:8000/transport/`)
+    .then(response=>response.json())
+    .then(data=>{
+      setTransports(data)
+    })
+    .catch(error=>console.log(error));
+  }, []);
 
-    return (
-      <div>
-        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading......'}
-      </div>
+  const displayTransport=() =>{
+    return transports.map((transport) => 
+    <div key={transport.id} className="transport-card">
+     <p className="transport-info">Bus: {transport.bus}</p>
+     <p className="transport-info">Departure: {transport.route_from}</p>
+     <p className="transport-info">Destination: {transport.route_to}</p>
+    </div>
     );
-}
+  }
+
+  return (
+    <div>
+      <h1 className="transport-title">Transport Information</h1>
+      <div className="transport-container">
+        {displayTransport()}
+      </div>
+    </div>
+  );
+};
 
 export default Transport;

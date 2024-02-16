6e6ba57d0986708b680055233c7e491104307738
Sruthi Santhosh <sruthi.santhosh2@mail.dcu.ie>
2024-02-16 00:35:32 +0000

    modified the logo and fixed navbar

--- code/frontend/atlas/src/components/navbar.jsx
+++ code/frontend/atlas/src/components/navbar.jsx
@@ -205,6 +205,124 @@
 
 
 
+// import React, { useState, useEffect } from 'react';
+// import logo from '../logo.png';
+
+// function Navbar() {
+//   const [isScrollingUp, setIsScrollingUp] = useState(true);
+//   const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+
+//   const handleScroll = () => {
+//     const currentScrollPos = window.pageYOffset;
+//     setIsScrollingUp(prevScrollPos > currentScrollPos);
+//     setPrevScrollPos(currentScrollPos);
+//   };
+
+//   useEffect(() => {
+//     window.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       <style>
+//         {`
+//           * {
+//             margin: 0;
+//             padding: 0;
+//             box-sizing: border-box;
+//             font-family: sans-serif;
+//           }
+//           body {
+//             position; fixed;
+//             // display: flex;
+//             justify-content: center;
+//             align-items: center;
+//             min-height: 100vh;
+//             background: #345; /* Body background color */
+//             margin: 0;
+//             padding: 3px 2px;
+//             color: #000;
+//           }
+//           nav {
+//             position: fixed;
+//             top: 0;
+//             left: 0%;
+//             width: 100%;
+//             background-color: #000; /* Fixed navbar background color */
+//             padding: 10px 20px;
+//             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+//             z-index: 1000;
+//           }
+//           nav a {
+//             position: relative;
+//             font-size: 1cm;
+//             font-weight: 100;
+//             color: #fff; /* Text color */
+//             text-decoration: none;
+//             padding: 6px 15px;
+//             transition: 0.5s;
+//           }
+//           nav a:hover {
+//             color: #8ef;
+//           }
+//           nav a span {
+//             position: absolute;
+//             top: 0;
+//             left: 0;
+//             width: 100%;
+//             height: 100%;
+//             z-index: -1;
+//             border-bottom: 2px solid #8ef; /* Border color on hover */
+//             border-radius: 15px;
+//             transform: scale(0) translateY(50px);
+//             opacity: 0;
+//             transition: 0.5s;
+//           }
+//           nav a:hover span {
+//             transform: scale(1) translateY(0);
+//             opacity: 1;
+//           }
+//           // .logo {
+//           //   display: inline-block;
+//           //   width: 40px; /* Adjust the width of your logo */
+//           //   height: auto;
+//           //   margin-right: 10px; /* Adjust the spacing between the logo and other links */
+//           // }
+//           // .logo {
+//           //   width: 100px; /* Adjust the width of the logo */
+//           //   height: 50px; /* Maintain aspect ratio */
+//           //   margin-right: 10px; /* Adjust spacing if needed */
+//           // }
+//           .logo {
+//             height: 50px; /* Set the height of the logo to match the navbar height */
+//             margin-right: 10px; /* Adjust spacing if needed */
+//           }
+//         `}
+//       </style>
+//       <nav className="navbar">
+//         {/* Your navbar content goes here */}
+//         <a href="/"><img src= {logo} alt="Logo" className="logo" /></a>
+//         <a href="searcharea">Search<span></span></a>
+//         <a href="/amenitiesnav">Amenities<span></span></a>
+//         <a href="/crimenav">Crime<span></span></a>
+//         <a href="/transportnav">Transport<span></span></a>
+//         <a href="/schoolsnav">Schools<span></span></a>
+//         <a href="/housenav">Housing<span></span></a>
+//         {/* <a href="/comment">Comments<span></span></a> */}
+//         <a href="#">Contact<span></span></a>
+//       </nav>
+//     </>
+//   );
+// }
+
+// export default Navbar;
+
+
+
 import React, { useState, useEffect } from 'react';
 import logo from '../logo.png';
 
@@ -234,37 +352,34 @@ function Navbar() {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-            font-family: sans-serif;
+            font-family: 'Lobster', cursive;
           }
           body {
-            position; fixed;
-            // display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            background: #345; /* Body background color */
+            background: linear-gradient(135deg, #283048 0%, #859398 100%);
+
             margin: 0;
-            padding: 3px 2px;
-            color: #000;
+            padding: 0;
+            color: #fff;
           }
           nav {
             position: fixed;
             top: 0;
-            left: 0%;
+            left: 0;
             width: 100%;
-            background-color: #000; /* Fixed navbar background color */
+            background-color: #000;
             padding: 10px 20px;
             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
             z-index: 1000;
+            border-radius: 0 0 20px 20px;
           }
           nav a {
-            position: relative;
-            font-size: 1cm;
-            font-weight: 100;
-            color: #fff; /* Text color */
+            font-size: 2rem;
+            font-weight: 400;
+            color: #fff;
             text-decoration: none;
-            padding: 6px 15px;
-            transition: 0.5s;
+            // margin-right: 15px;
+            padding: 4px 15px;
+            transition: color 0.3s ease;
           }
           nav a:hover {
             color: #8ef;
@@ -276,44 +391,32 @@ function Navbar() {
             width: 100%;
             height: 100%;
             z-index: -1;
-            border-bottom: 2px solid #8ef; /* Border color on hover */
+            border-bottom: 2px solid #8ef;
             border-radius: 15px;
             transform: scale(0) translateY(50px);
             opacity: 0;
-            transition: 0.5s;
+            transition: transform 0.5s, opacity 0.5s;
           }
           nav a:hover span {
             transform: scale(1) translateY(0);
             opacity: 1;
           }
-          // .logo {
-          //   display: inline-block;
-          //   width: 40px; /* Adjust the width of your logo */
-          //   height: auto;
-          //   margin-right: 10px; /* Adjust the spacing between the logo and other links */
-          // }
-          // .logo {
-          //   width: 100px; /* Adjust the width of the logo */
-          //   height: 50px; /* Maintain aspect ratio */
-          //   margin-right: 10px; /* Adjust spacing if needed */
-          // }
           .logo {
-            height: 50px; /* Set the height of the logo to match the navbar height */
-            margin-right: 10px; /* Adjust spacing if needed */
+            width: 100%;
+            height: 60px;
+            margin-right: 10px;
           }
         `}
       </style>
       <nav className="navbar">
-        {/* Your navbar content goes here */}
-        <a href="/"><img src= {logo} alt="Logo" className="logo" /></a>
+        <a href="/"><img src={logo} alt="Logo" className="logo" /></a>
         <a href="searcharea">Search<span></span></a>
         <a href="/amenitiesnav">Amenities<span></span></a>
         <a href="/crimenav">Crime<span></span></a>
         <a href="/transportnav">Transport<span></span></a>
         <a href="/schoolsnav">Schools<span></span></a>
         <a href="/housenav">Housing<span></span></a>
-        {/* <a href="/comment">Comments<span></span></a> */}
-        <a href="#">Contact<span></span></a>
+        <a href="/contact">Contact<span></span></a>
       </nav>
     </>
   );

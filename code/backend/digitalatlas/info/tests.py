53b30be22586643a5f920cf409891ac254c95dcf
Sruthi Santhosh <sruthi.santhosh2@mail.dcu.ie>
2024-02-22 12:19:53 +0000

    django testing completed using pytest

--- code/backend/digitalatlas/info/tests.py
+++ code/backend/digitalatlas/info/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
-
 # Create your tests here.
+
+import os
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'digitalatlas.settings')
+
+import pytest
+from django.urls import reverse
+from rest_framework.test import APIClient
+from .models import *
+from .serializers import *
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.mark.django_db
+def test_transport_list(api_client):
+    url = reverse('transport-list')
+    response = api_client.get(url)
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_house_list(api_client):
+    url = reverse('house-list')
+    response = api_client.get(url)
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_extra_transport_list(api_client):
+    url = reverse('extra_transport-list')
+    response = api_client.get(url)
+    assert response.status_code == 200
+
+
+
